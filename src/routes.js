575eda0c288ce7683a3f2c2d2a2dbf432d5cba0e
Your Name <carlao.linux@gmail.com>
2019-11-02 12:09:37 -0300

    Desafio completo

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
-import SessionController from './app/controllers/SessionController'
+import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/session', SessionController.store)
+routes.get('/session', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/student', StudentController.store);
+routes.put('/student/:id', StudentController.update);
 
 export default routes;
